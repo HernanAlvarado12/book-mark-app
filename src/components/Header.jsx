73b1e47ef751722e2bf73890ba04ead0f28fc5a8
halvaradop_ <hernanvid123@gmail.com>
2023-05-26 20:57:04 -0500

    styles to desktop device in header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,18 +24,18 @@ const Header = () => {
     }
 
     return (
-        <header>
-            <nav className="w-90 h-9 mx-auto flex items-center justify-between">
+        <header className="base:w-85 base:h-9 base:mx-auto base:flex base:items-center base:justify-between">
+            <nav className="w-90 h-9 mx-auto flex items-center justify-between base:w-auto base:h-auto base:m-0">
                 <img src={navbarImages.logo} alt="logo icon" />
-                <img className="cursor-pointer" src={navbarImages.menu} alt="menu icon" ref={menuIcon} onClick={handleMenuToggle} />
+                <img className="cursor-pointer base:hidden" src={navbarImages.menu} alt="menu icon" ref={menuIcon} onClick={handleMenuToggle} />
             </nav>
-            <aside className="w-full hidden absolute inset-y-0 right-0 bg-blue-200/95" ref={menuRef}>
-                <menu className="w-90 h-screen mx-auto pt-5 pb-8 flex items-center justify-between flex-col">
-                    <ul className="w-full mt-6 mb-auto text-center">
-                        {headerList.map((item, key) => <Item key={key} className="uppercase" text={item} color="white-100" size="bs" />)}
-                        <Button className="w-full mt-3 uppercase tracking-widest" text="login" color="transparent" size="md" />
+            <aside className="w-full hidden absolute inset-y-0 right-0 bg-blue-200/95 base:block base:relative base:inset-auto base:bg-transparent" ref={menuRef}>
+                <menu className="w-90 h-screen mx-auto pt-5 pb-8 flex items-center justify-between flex-col base:w-full base:h-auto base:p-0 base:flex-row">
+                    <ul className="w-full max-w-4xl mt-6 mb-auto flex items-center flex-col text-center base:w-full base:max-w-none base:m-0 base:flex-row base:justify-end base:gap-x-3">
+                        {headerList.map((item, key) => <Item key={key} className="w-full uppercase base:w-auto base:text-blue-200 base:text-sm base:child:border-0" text={item} color="white-100" size="bs" />)}
+                        <Button className="w-full mt-3 uppercase tracking-widest base:w-15 base:h-4 base:m-0 base:bg-red" text="login" color="transparent" size="md" />
                     </ul>
-                    <LinkCompany className="flex items-center gap-x-2" companies={metaCompanies} />
+                    <LinkCompany className="flex items-center gap-x-2 base:hidden" companies={metaCompanies} />
                 </menu>
             </aside>
         </header>
